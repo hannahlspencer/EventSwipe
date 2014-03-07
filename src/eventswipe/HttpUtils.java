cd1f754a8a4bdd72e8c1751556f877430e8ef77d
Matt Wildman <matthewwildman@gmail.com>
2014-03-07 16:53:52 +0000

    Started work on CareerHub API and HttpUtils

---
+++ src/eventswipe/HttpUtils.java
@@ -0,0 +1,27 @@
+package eventswipe;
+
+import java.util.Map;
+
+public class HttpUtils {
+
+    public void connectToURL(String url) {
+        
+    }
+    
+    public void closeConnection(String url) {
+        
+    }
+
+    public void closeAllConnections() {
+        
+    }
+
+    public void sendDataToURL(String url, String method, String data, Map headers) {
+
+    }
+
+    public String getDataFromURL(String url, String method, String data, Map headers) {
+        return "";
+    }
+
+}
