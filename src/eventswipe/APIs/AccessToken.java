0d5cee58fe7db69b80badf71950973cebe90330e
Matt Wildman <matthewwildman@gmail.com>
2016-07-28 17:43:36 +0100

    Token management with AccessToken class

---
+++ src/eventswipe/APIs/AccessToken.java
@@ -0,0 +1,72 @@
+package eventswipe.APIs;
+
+import java.util.Date;
+
+/**
+ *
+ * @author wildmanm
+ */
+public class AccessToken {
+
+    /**
+     * @return the token
+     */
+    public String getToken() {
+        return token;
+    }
+
+    /**
+     * @param token the token to set
+     */
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    /**
+     * @return the scope
+     */
+    public String getScope() {
+        return scope;
+    }
+
+    /**
+     * @param scope the scope to set
+     */
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    /**
+     * @return the expiryDate
+     */
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    /**
+     * @param expiryDate the expiryDate to set
+     */
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    /**
+     * @return the tokenType
+     */
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    /**
+     * @param tokenType the tokenType to set
+     */
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    private String token;
+    private String scope;
+    private Date expiryDate;
+    private String tokenType;
+
+}
